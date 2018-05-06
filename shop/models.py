4409d652e5c18815e6df00e68f2556a6d4b9e439
Louay Hamada <loauy.hamada@gmail.com>
2018-05-06 22:37:17 +0300

    Lesson 02: Mission

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+  name = models.CharField(max_length=100)
+  description = models.TextField(blank=True, max_length=256)
+  slug = models.SlugField(unique=True, blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  class Meta:
+    ordering = ('name',)
+
+  def __str__(self):
+    return self.name
+
 class Product(models.Model):
   name = models.CharField(max_length=100)
   price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -8,6 +20,7 @@ class Product(models.Model):
   created_at = models.DateTimeField(auto_now_add=True)
   description = models.TextField(blank=True)
   slug = models.SlugField(unique=True, blank=True)
+  category = models.ForeignKey(Category, related_name='products', default=1, on_delete=models.CASCADE)
 
   class Meta:
     ordering = ('name',)
