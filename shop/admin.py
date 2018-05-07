b72ed960c2860739970e318f5aef84465628809b
Louay Hamada <loauy.hamada@gmail.com>
2018-05-07 23:03:08 +0300

    Lesson 04: Mission

--- shop/admin.py
+++ shop/admin.py
@@ -6,12 +6,23 @@ admin.site.site_header = "Coretabs"
 admin.site.index_title = "Coretabs Online Shop"
 admin.site.site_title = "Administration"
 
-admin.site.register(models.Category)
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+    search_fields = ['name']
+    list_display = ('name', 'description',)
 
 def zerofy(modeladmin, request, queryset):
     queryset.update(price=0)
 
 zerofy.short_description = "Zerofy selected products"
+
+def discount(modeladmin, request, queryset):
+    for product in queryset:
+        product.price = product.price * 80 / 100
+        product.save()
+
+discount.short_description = "20 percent discount selected products"
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -19,4 +30,4 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields = ['name']
     list_display = ('name', 'price', 'stock', 'category',)
     list_filter = ('created_at', 'category',)
-    actions = [zerofy]
+    actions = [zerofy, discount]
